b5f85e71d93e94140df57765bbca6084989f5a88
vohoangloc123 <you@example.com>
2024-08-19 21:42:32 +0700

    Handled displaying different screens based on authentication state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/chat.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // initialize the app
@@ -26,7 +28,20 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance
+              .authStateChanges(), // listen to auth state changes
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+            return const AuthScreen();
+          }),
     );
   }
 }
