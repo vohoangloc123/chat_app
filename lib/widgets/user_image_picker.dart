bb5424c481c804b146ec5c3113e91d4390041ffd
vohoangloc123 <you@example.com>
2024-08-19 23:35:42 +0700

    Added and used the ImagePicker Package

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   const UserImagePicker({super.key});
@@ -8,14 +11,27 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickImageFile;
+  void _pickImage() async {
+    final pickedImage = await ImagePicker()
+        .pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150);
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _pickImageFile = File(pickedImage!.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const CircleAvatar(
+        CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          backgroundImage: null,
+          backgroundImage:
+              _pickImageFile != null ? FileImage(_pickImageFile!) : null,
         ),
         TextButton.icon(
           icon: const Icon(Icons.image),
@@ -25,7 +41,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
               color: Theme.of(context).colorScheme.primary,
             ),
           ),
-          onPressed: () {},
+          onPressed: _pickImageFile != null ? null : _pickImage,
         ),
       ],
     );
